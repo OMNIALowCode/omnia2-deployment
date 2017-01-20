afc1efc4d2eccf010e058604af6dc1fc55ef6326
josemaia <josecbmaia@hotmail.com>
2017-01-20 10:42:11 +0000

    Add sample of calling update on all sites in a subscription

---
+++ MassUpdateSample.ps1
@@ -0,0 +1,22 @@
+﻿Param(
+    [string] [Parameter(Mandatory=$true)] $SubscriptionName, #as in -subscriptionname param elsewhere
+    [string] $updateScriptLocation = ".\OmniaUpdate.ps1"
+)
+Select-AzureRmSubscription -SubscriptionName  $SubscriptionName
+
+$sitesList = New-Object System.Collections.Generic.List[System.Object]
+
+ foreach ($group in $groups){ 
+    $a = Get-AzureRmResource -ResourceType "Microsoft.Web/sites" -ResourceGroupName $group.ResourceGroupName;
+    foreach ($site in $a){
+        if ($site.Name -ne $snull -and $site.Name -notmatch "extensibility") { #do not want to update any Node websites!
+            $sitesList.Add($site)
+        }
+    }
+  }
+
+foreach ($site in $sitesList){
+    $params = "-ResourceGroupName `""+($site.ResourceGroupName)+"`" -WebsiteName `"https://"+$site.Name+".azurewebsites.net`" -SubscriptionName `"$SubscriptionName`" -Force "
+    Write-Host ("Calling OmniaUpdate.ps1 with params: " + $params)
+    Invoke-Expression "& `"$updateScriptLocation`" $params"
+}
